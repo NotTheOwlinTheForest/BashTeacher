0221cc9419c18283b967fbea3d56457df6bb10e2
NotTheOwlinTheForest <31040102+NotTheOwlinTheForest@users.noreply.github.com>
2017-08-16 09:35:12 +0200

    Add files via upload
    I added my code

---
+++ BashTeacher.sh
@@ -0,0 +1,278 @@
+echo "" > ErsteLektion.sh
+echo ""
+echo "Heute wirst du bash lernen!"
+
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+
+echo ""
+echo "mach ein rechts click auf die Datei 'ErsteLektion.sh'"
+echo ""
+echo "drücke Öffne mit Notepad ++"
+
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+
+echo ""
+echo "Nun können wir ein kleines Programm schreiben, welches Hallo sagt."
+
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+
+echo ""
+echo "Schreibe folgendes: "
+echo ""
+echo 'echo "Hallo" '
+
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+
+echo ""
+echo "Danach drücke CTRL S"
+
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+
+echo ""
+echo "Schliesse Notepad ++ und doppelklicke ErsteLektion.sh"
+
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+
+echo "Wie du gesehen hast, hat sich die Applikation geschlossen"
+
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+
+echo ""
+echo 'echo "Hallo"
+sleep 3s' > ErsteLektion.sh
+echo "Führe die Datei ErsteLektion.sh nochmal aus."
+
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+echo "Wie du gesehen hast, blieb jetzt das Fenster 3 Sekunden offen."
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+echo "Öffne ErsteLektion.sh mit Notepad++ noch einmal und sieh dir den Code an."
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+echo "Wie du siehst, habe ich dir hier ein wenig geholfen."
+echo ""
+sleep 1s
+echo "Ich habe hier sleep 3s hinzugefügt, damit das Fenster am Ende offen bleibt."
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+echo "Das ist aber nur auf Windows nötig. Bei Linux bleibt das Terminal offen bis du es selber schliesst."
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+echo 'Lösche mal alles und schreibe selber ein paar Worte mit echo "DEIN TEXT"'
+echo ""
+sleep 1s
+echo "Tipp: verwende sleep, damit das Terminal offen bleibt."
+echo ""
+echo ""
+read -p "Enter wenn es einwandfrei funktioniert hat" dasd
+clear
+echo ""
+echo "GUT GEMACHT! DU HAST DIE ERSTE LEKTION BESTANDEN!"
+sleep 2s
+echo ""
+echo '░░░░░░░░░░░████░░░░░░░░░░░░░░░░░░░░'
+sleep 0.5s
+echo '░░░░░░░░░███░██░░░░░░░░░░░░░░░░░░░░'
+sleep 0.5s
+echo '░░░░░░░░░██░░░█░░░░░░░░░░░░░░░░░░░░' 
+sleep 0.5s
+echo '░░░░░░░░░██░░░██░░░░░░░░░░░░░░░░░░░' 
+sleep 0.5s
+echo '░░░░░░░░░░██░░░███░░░░░░░░░░░░░░░░░' 
+sleep 0.5s
+echo '░░░░░░░░░░░██░░░░██░░░░░░░░░░░░░░░░' 
+sleep 0.5s
+echo '░░░░░░░░░░░██░░░░░███░░░░░░░░░░░░░░' 
+sleep 0.5s
+echo '░░░░░░░░░░░░██░░░░░░██░░░░░░░░░░░░░' 
+sleep 0.5s
+echo '░░░░░░░███████░░░░░░░██░░░░░░░░░░░░' 
+sleep 0.5s
+echo '░░░░█████░░░░░░░░░░░░░░███░██░░░░░░' 
+sleep 0.5s
+echo '░░░██░░░░░████░░░░░░░░░░██████░░░░░' 
+sleep 0.5s
+echo '░░░██░░████░░███░░░░░░░░░░░░░██░░░░' 
+sleep 0.5s
+echo '░░░██░░░░░░░░███░░░░░░░░░░░░░██░░░░' 
+sleep 0.5s
+echo '░░░░██████████░███░░░░░░░░░░░██░░░░' 
+sleep 0.5s
+echo '░░░░██░░░░░░░░████░░░░░░░░░░░██░░░░' 
+sleep 0.5s
+echo '░░░░███████████░░██░░░░░░░░░░██░░░░' 
+sleep 0.5s
+echo '░░░░░░██░░░░░░░████░░░░░██████░░░░░' 
+sleep 0.5s
+echo '░░░░░░██████████░██░░░░███░██░░░░░░' 
+sleep 0.5s
+echo '░░░░░░░░░██░░░░░████░███░░░░░░░░░░░' 
+sleep 0.5s
+echo '░░░░░░░░░█████████████░░░░░░░░░░░░░' 
+sleep 0.5s
+echo '░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░'
+sleep 3s
+echo "" > ZweiteLektion.sh 
+echo ""
+echo ""
+echo "nun schauen wir uns user input an und wie wir ihn speichern"
+
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+
+echo "Öffne mit Notepad++ ZweiteLektion.sh"
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+
+echo "Schreibe folgendes:"
+echo ""
+echo 'read -p "Input:" Wort 
+echo $Wort
+read -p "Enter für weiter" dasd' 
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+echo "Führe nun ZweiteLektion.sh aus"
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+echo "SUPER! jetzt fragen wir dich nach deinem Namen"
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+echo "Öffne ZweiteLektion.sh mit Notepad++ nochmals"
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+echo "Zuerst möchte ich, dass du vor deinen Code überall # hinschreibst"
+echo ""
+echo ""
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+echo "so sollte es aussehen: "
+echo ""
+echo '
+#read -p "Input: " Wort
+#echo $Wort
+#read -p "Enter für weiter" dasd'
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+echo "Du hast jetzt deinen vorherigen Code auskommentiert!"
+echo ""
+echo "Nun können wir fortfahren.."
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+echo "Schreibe folgendes unten an deinen Auskommentierten code:"
+echo ""
+echo '
+read -p "Hallo wie heisst du? " Eingabe 
+echo ""
+echo "Hallo $Eingabe ! Mir gefällt dein Name"
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+'
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+echo "" > Pinger.sh
+echo "Nun will ich, das du etwas für mich programmierst."
+sleep 0.5s
+echo ""
+echo "Natürlich werde ich dir ein Tipp geben."
+sleep 0.5s
+echo ""
+echo "Ich will das du ein Programm machst,"
+echo ""
+sleep 0.5s
+echo "welches eine Webseite oder eine IP beliebig anpingt."
+echo ""
+sleep 0.5s
+echo "Der Tipp: "
+echo ""
+sleep 0.5s
+echo "ping www.google.com ODER ping 127.0.0.1"
+echo ""
+sleep 0.5s
+echo "Benutze Pinger.sh"
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+echo "Gut. Nun sehen wir jedoch, dass der Pinger nicht unendlich Pingt."
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+echo "Jetzt kommen wir zu Schlaufen oder Loops"
+echo ""
+echo ""
+read -p "Enter für weiter" dasd
+clear
+echo ""
+echo "ich werde dir jetzt mal schnell einen Loop aufschreiben"
